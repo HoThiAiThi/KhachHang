bcef5e8903f5878388c9eb0184d6d9cc9e792a2f
HoThiAiThi <hothiaithi123@gmail.com>
2022-04-13 08:09:38 +0700

    first commit

---
+++ KhachHang/KhachHang/src/Dao/Connect.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+/**
+ *
+ * @author win
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author DELL
+ */
+public class Connect {
+
+     public static Connection getJDBCConection() {
+        final String url = "jdbc:mysql://localhost:3306/KhachHang";
+        final String user = "sa";
+        final String password = "051202";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+}
